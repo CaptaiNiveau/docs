6a13cf1526b0a2ccfcb5f8c218447d2d1857ffc4
aronmal <aron@mal-noh.de>
2022-07-14 23:55:12 +0200

    Initialization

---
+++ docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set variables
+name=docs
+author=CaptaiNiveau
+github=CaptaiNiveau/docs
+version=$(curl -s https://api.github.com/repos/${github}/releases/latest | grep 'tag_name' | cut -d\" -f4)
+packageVER=${version}
+packageName=${name}-package-${packageVER}
+packagefile=${packageName}.zip
+packageURL=https://github.com/${github}/releases/download/${version}/${packagefile}
+
+# Execute
+mkdir /github
+cd /github
+wget ${packageURL}
+unzip ${packagefile}
+rm -r ${packagefile}
